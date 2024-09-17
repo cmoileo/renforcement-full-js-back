40e485293f6224f2f9597e2cb6df7fd3c220f365
cmoileo <leo.fezard33@gmail.com>
2024-09-17 09:44:24 +0200

    add rows + data in db

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ let db = new sqlite3.Database('./mydb.sqlite3', (err) => {
     console.log('Connected to the SQLite database.');
 });
 
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS contacts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE
+    )`, (err) => {
+        if (err) {
+            // La table existe déjà
+        } else {
+            // Table fraîchement créer, ajouter la data
+            const insert = 'INSERT OR IGNORE INTO contacts (name, email) VALUES (?,?)';
+            db.run(insert, ["contact1","contact1@example.com"]);
+            db.run(insert, ["contact2","contact2@example.com"]);
+        }
+    });
+
+    db.all("SELECT * FROM contacts", [], (err, rows) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        console.log(rows)
+    });
+});
+
 const express = require('express');
 const app = express();
 
